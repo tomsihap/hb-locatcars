1841e45a46b4828cc4f38b6cc0cad6420a62a0eb
Thomas Sihapanya <sihapanya.thomas@gmail.com>
2020-02-11 10:15:01 +0100

    First template

--- config/routes.php
+++ config/routes.php
@@ -3,5 +3,4 @@
 $router = $container->getRouter();
 $router->setNamespace('App\Controller');
 $router->get('/cars', 'CarsController@index');
-
 $router->run();
--- src/controller/CarsController.php
+++ src/controller/CarsController.php
@@ -5,6 +5,22 @@ namespace App\Controller;
 class CarsController {
 
     public function index() {
+
+        $cars = [
+            [
+                "brand" => "Ford",
+                "model" => "Fiesta",
+            ],
+            [
+                "brand" => "Citroën",
+                "model" => "C3",
+            ],
+        ];
+
         include_once __DIR__ . '/../../template/cars/index.php';
+    }
+
+    public function show(int $id) {
+        echo "Voici la voiture numéro " . $id ;
     }
 }
